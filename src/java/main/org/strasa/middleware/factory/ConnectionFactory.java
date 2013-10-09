4417afa8b8a605d231d6716e429dae551bfdf2a0
acanedairri <a.caneda@irri.org>
2013-10-09 15:22:51 +0800

    Initial

---
+++ src/java/main/org/strasa/middleware/factory/ConnectionFactory.java
@@ -0,0 +1,93 @@
+package org.strasa.middleware.factory;
+ 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.strasa.middleware.mapper.CountryMapper;
+import org.strasa.middleware.mapper.EcotypeMapper;
+import org.strasa.middleware.mapper.GeorefMapper;
+import org.strasa.middleware.mapper.GermplasmCharacteristicsMapper;
+import org.strasa.middleware.mapper.GermplasmMapper;
+import org.strasa.middleware.mapper.GermplasmReleaseInfoMapper;
+import org.strasa.middleware.mapper.LocationMapper;
+import org.strasa.middleware.mapper.PlantingTypeMapper;
+import org.strasa.middleware.mapper.ProgramMapper;
+import org.strasa.middleware.mapper.ProjectMapper;
+import org.strasa.middleware.mapper.StudyAgronomyMapper;
+import org.strasa.middleware.mapper.StudyDerivedDataMapper;
+import org.strasa.middleware.mapper.StudyDesignMapper;
+import org.strasa.middleware.mapper.StudyFileMapper;
+import org.strasa.middleware.mapper.StudyLocationMapper;
+import org.strasa.middleware.mapper.StudyMapper;
+import org.strasa.middleware.mapper.StudyRawDataMapper;
+import org.strasa.middleware.mapper.StudySiteMapper;
+import org.strasa.middleware.mapper.StudyTypeMapper;
+import org.strasa.middleware.mapper.StudyVariableMapper;
+import org.strasa.middleware.mapper.UserMapper;
+
+ 
+public class ConnectionFactory {
+ 
+
+
+    private static SqlSessionFactory sqlSessionFactory;
+ 
+    
+    
+    public ConnectionFactory(){
+    
+    
+            String resource = "SqlMapConfig.xml";
+            Reader reader;
+			try {
+				reader = Resources.getResourceAsReader(resource);
+				 if (sqlSessionFactory == null) {
+		                sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		                sqlSessionFactory.getConfiguration().addMapper(CountryMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(EcotypeMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(GeorefMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(GermplasmCharacteristicsMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(GermplasmMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(GermplasmReleaseInfoMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(GermplasmMapper.class);  
+		                sqlSessionFactory.getConfiguration().addMapper(LocationMapper.class);  
+		                sqlSessionFactory.getConfiguration().addMapper(PlantingTypeMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(ProgramMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(ProjectMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyAgronomyMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyDerivedDataMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyDesignMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyFileMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyLocationMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyRawDataMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudySiteMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyTypeMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(StudyVariableMapper.class);
+		                sqlSessionFactory.getConfiguration().addMapper(UserMapper.class);
+
+//		                sqlSessionFactory.getConfiguration().addMapper();
+		            
+		            }
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+ 
+           
+    }
+ 
+    public SqlSessionFactory getSqlSessionFactory() {
+ 
+        return sqlSessionFactory;
+    }
+    /**
+     * Returns a DataSource object.
+     *
+     * @return a DataSource.
+     */
+   
+}
---
+++ src/java/main/org/strasa/web/ui/controller/UploadDataController.java
@@ -0,0 +1,99 @@
+package org.strasa.web.ui.controller;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import org.zkoss.bind.BindUtils;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.event.UploadEvent;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+public class UploadDataController extends SelectorComposer<Window> {
+	
+
+	@Wire("#lblMsgUpload")
+	private Label lblUploadMessage;
+	
+	
+
+	@Listen("onUpload = #uploadCSV")
+	public void updata(UploadEvent event) {
+	   
+		if(event.getMedia().isBinary()){
+			 Messagebox.show("Error: File must be a text-based csv format", "Upload Error", Messagebox.OK, Messagebox.ERROR);
+			 return;
+		}
+		
+//		System.out.println(event.getMedia().getStringData());
+		
+		
+		String name = "sampleName";
+	
+	    String path = Sessions.getCurrent().getWebApp().getRealPath("/UPLOADS/") + "/";                
+	   
+
+	    this.uploadFile(path, name, ".csv", event.getMedia().getStringData());       
+	        BindUtils.postNotifyChange(null, null, this, "*");
+	        
+
+		    
+	        lblUploadMessage.setValue("Filename: " + name + ".csv");
+		
+	}
+	public void uploadFile(String path, String name, String ext,
+           String data) {
+ 
+		String filePath = path + name + ext;
+		
+		try {
+			PrintWriter out = new PrintWriter(filePath);
+			out.println(data);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+	
+	public boolean validateHeader(String path){
+		
+		//STATIC
+		HashMap<String,String> headerData = new HashMap<String,String>();
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		headerData.put("GN", "GName");
+		
+		try {
+			CSVReader reader = new CSVReader(new FileReader(path));
+			String[] header = reader.readNext();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+		
+	}
+}
