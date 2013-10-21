56ee94cbbac63e6f30fc2a5c77d4ccbcd4058e1b
acanedairri <a.caneda@irri.org>
2013-10-21 11:03:58 +0800

    added delete user method

--- src/java/main/org/strasa/web/admin/view/model/UserManagement.java
+++ src/java/main/org/strasa/web/admin/view/model/UserManagement.java
@@ -7,34 +7,47 @@ import org.strasa.middleware.model.User;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zul.Messagebox;
 
 public class UserManagement {
 
 
-	List<User> user;
+	List<User> users;
 
 	public List<User> getUser() {
-		return user = this.getAllRegisteredUser();
+		return users = this.getAllRegisteredUser();
 	}
 
-	public void setUser(List<User> user) {
-		this.user = user;
-	}
 
 	public List<User> getAllRegisteredUser(){
-		
+
 		UserManagerImpl userManagerImpl= new UserManagerImpl();
 		List<User> users=userManagerImpl.getAllRegisteredUser();
 		return users;
-		
+
 	}
-	
+
 	@NotifyChange("*")
 	@Command("onChecked")
-	public void addProject(@BindingParam("user") User user){
+	public void addUser(@BindingParam("user") User user){
 		UserManagerImpl userManagerImp =  new UserManagerImpl();
 		userManagerImp.updateUser(user);
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@NotifyChange("*")
+	@Command("deleteUser")
+	public void deleteUser(@BindingParam("user") final User user){
+		
+		Messagebox.show("Are you sure to delete this user?", "Confirm Dialog", Messagebox.OK | Messagebox.CANCEL, Messagebox.QUESTION, new org.zkoss.zk.ui.event.EventListener() {
+			public void onEvent(Event evt) throws InterruptedException {
+				if (evt.getName().equals("onOK")) {
+					UserManagerImpl userManagerImp =  new UserManagerImpl();
+					userManagerImp.deleteUser(user);
+				} 
+			}
+		});
+	}
 
 }
