6b820367266d4f123e540169d2c4030879c5d8de
nmtimbol <nmtimbol@AUNT0150>
2013-11-08 17:05:21 +0800

    update

--- src/java/main/org/strasa/web/germplasmquery/view/model/GermplasmSearchTable.java
+++ src/java/main/org/strasa/web/germplasmquery/view/model/GermplasmSearchTable.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import org.strasa.middleware.manager.GermplasmQueryManagerImpl;
 import org.strasa.middleware.model.Germplasm;
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
 
 public class GermplasmSearchTable {
 	    private GermplasmQueryManagerImpl germplasmQueryMan = new GermplasmQueryManagerImpl();
-	    private GermplasmSearchResultModel searchFilter = new GermplasmSearchResultModel();
-	    private List<Germplasm> germplasmnames = germplasmQueryMan.initializeGermplasmQuerys(1);
 	    
+	    private GermplasmSearchResultModel searchFilter = new GermplasmSearchResultModel();
+	    
+	    private List<Germplasm> germplasmnames = germplasmQueryMan.initializeGermplasmQuerys(1);
+	    private Integer selectedSearchGermplasm = 0;
+	    private boolean angel = true;
 	    
 	    public List<Germplasm> getGermplasmnames() {
 			return germplasmnames;
@@ -23,4 +29,25 @@ public class GermplasmSearchTable {
 		public void setSearchFilter(GermplasmSearchResultModel searchFilter) {
 			this.searchFilter = searchFilter;
 		}
+		public Integer getSelectedSearchGermplasm() {
+			return selectedSearchGermplasm;
+		}
+		public void setSelectedSearchGermplasm(Integer selectedSearchGermplasm) {
+			this.selectedSearchGermplasm = selectedSearchGermplasm;
+		}
+		public boolean isAngel() {
+			return angel;
+		}
+		public void setAngel(boolean angel) {
+			this.angel = angel;
+		}
+
+		@Command("viewKeyChar")
+		@NotifyChange("*")
+		public void viewKeyChar(@BindingParam("selectedGermplasmSearch") String selected){
+			System.out.println("selected "+ selected);
+			if(selected.equals("Name")) angel=true;
+			else angel=false;
+		}
+	
 	}
