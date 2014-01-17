fcb17572b57b64f74e413294a9fbe9372c962f5d
Alex <a.caneda@irri.org>
2014-01-17 11:39:20 +0800

    update ui

--- src/main/java/org/strasa/web/page/navigation/user/SidebarPageConfigAjaxBasedImpl.java
+++ src/main/java/org/strasa/web/page/navigation/user/SidebarPageConfigAjaxBasedImpl.java
@@ -20,7 +20,7 @@ public class SidebarPageConfigAjaxBasedImpl implements SidebarPageConfig{
 	
 	HashMap<String,SidebarPage> pageMap = new LinkedHashMap<String,SidebarPage>();
 	public SidebarPageConfigAjaxBasedImpl(){		
-		pageMap.put("upload_menu1",new SidebarPage("upload_menu1","New Study","/images/fn.png","/user/uploadstudy/index.zul"));
+//		pageMap.put("upload_menu1",new SidebarPage("upload_menu1","New Study","/images/fn.png","/user/uploadstudy/index.zul"));
 		pageMap.put("upload_menu2",new SidebarPage("upload_menu2","Study","/images/study16.png","/user/maintenance/edituploadedstudies.zul"));
 		pageMap.put("upload_menu3",new SidebarPage("upload_menu2","Germplasm","/images/Germplasm16.png","/user/home.zul"));
 		pageMap.put("upload_menu4",new SidebarPage("upload_menu4","Genotyping Data","/images/fn.png","/user/home.zul"));
